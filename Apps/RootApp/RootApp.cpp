ea2ebd841116e665f3f5817e23d6ee36d85e22a2
cormoran <cormoran707@gmail.com>
2014-11-07 02:26:56 +0900

    modify

--- Apps/RootApp/RootApp.cpp
+++ Apps/RootApp/RootApp.cpp
@@ -2,6 +2,6 @@
 #include<cstdlib>
 int main()
 {
-  system("/home/rokko_oroshi/Rokko_Festival/Apps/Manager/Manager.out");
+  system("/home/rokko_oroshi/RokkoFestival/Apps/Manager/Manager.out");
   return 0;
 }
